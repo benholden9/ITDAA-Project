670ff0f1810e84d82e5ae24749118bb42c9c8837
Ben Holden <52157616+benholden9@users.noreply.github.com>
2024-06-24 18:13:44 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 import os
-from sklearn.ensemble import GradientBoostingClassifier as xgb
+import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
